668f21ca25312c3f613d28b9aea81cb544dd5663
mmlee0819 <mmlee0819@gmail.com>
2022-08-31 19:06:00 +0800

    Modify: week3_assignment3_request4 (Ajax)

--- week_3/my_first_web_server/app.js
+++ week_3/my_first_web_server/app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 const port = 3000
 
 app.use('/', express.static('public'))
 app.use(bodyParser.urlencoded({ extended: false }))
-
+app.use(cookieParser())
 
 app.set('view engine', 'pug')
 
